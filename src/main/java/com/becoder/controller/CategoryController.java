c95e76db5dfaef23ce1c913ae4631c0448e28a09
apna-colleage-demo <rahulmali2140@gmail.com>
2024-10-15 08:37:38 +0530

    add code controller

---
+++ src/main/java/com/becoder/controller/CategoryController.java
@@ -0,0 +1,82 @@
+package com.becoder.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.becoder.dto.CategoryDto;
+import com.becoder.dto.CategoryReponse;
+import com.becoder.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/v1/category")
+public class CategoryController {
+
+	@Autowired
+	private CategoryService categoryService;
+
+	@PostMapping("/save")
+	public ResponseEntity<?> saveCategory(@RequestBody CategoryDto categoryDto) {
+
+		Boolean saveCategory = categoryService.saveCategory(categoryDto);
+		if (saveCategory) {
+			return new ResponseEntity<>("saved success", HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<>("not saved", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<?> getAllCategory() {
+
+		List<CategoryDto> allCategory = categoryService.getAllCategory();
+		if (CollectionUtils.isEmpty(allCategory)) {
+			return ResponseEntity.noContent().build();
+		} else {
+			return new ResponseEntity<>(allCategory, HttpStatus.OK);
+		}
+
+	}
+
+	@GetMapping("/active")
+	public ResponseEntity<?> getActiveCategory() {
+
+		List<CategoryReponse> allCategory = categoryService.getActiveCategory();
+		if (CollectionUtils.isEmpty(allCategory)) {
+			return ResponseEntity.noContent().build();
+		} else {
+			return new ResponseEntity<>(allCategory, HttpStatus.OK);
+		}
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getCategortDetailsById(@PathVariable Integer id) {
+		CategoryDto categoryDto = categoryService.getCategoryById(id);
+		if (ObjectUtils.isEmpty(categoryDto)) {
+			return new ResponseEntity<>("Category not found with Id=" + id, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(categoryDto, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteCategoryById(@PathVariable Integer id) {
+		Boolean deleted = categoryService.deleteCategory(id);
+		if (deleted) {
+			return new ResponseEntity<>("Category deleted success", HttpStatus.OK);
+		}
+		return new ResponseEntity<>("Category Not deleted", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
---
+++ src/main/java/com/becoder/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,90 @@
+package com.becoder.service.impl;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import com.becoder.dto.CategoryDto;
+import com.becoder.dto.CategoryReponse;
+import com.becoder.entity.Category;
+import com.becoder.repository.CategoryRepository;
+import com.becoder.service.CategoryService;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepo;
+
+	@Autowired
+	private ModelMapper mapper;
+
+	@Override
+	public Boolean saveCategory(CategoryDto categoryDto) {
+
+//		Category category = new Category();
+//		category.setName(categoryDto.getName());
+//		category.setDescription(categoryDto.getDescription());
+//		category.setIsActive(categoryDto.getIsActive());
+
+		Category category = mapper.map(categoryDto, Category.class);
+
+		category.setIsDeleted(false);
+		category.setCreatedBy(1);
+		category.setCreatedOn(new Date());
+		Category saveCategory = categoryRepo.save(category);
+		if (ObjectUtils.isEmpty(saveCategory)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public List<CategoryDto> getAllCategory() {
+		List<Category> categories = categoryRepo.findAll();
+
+		List<CategoryDto> categoryDtoList = categories.stream().map(cat -> mapper.map(cat, CategoryDto.class)).toList();
+
+		return categoryDtoList;
+	}
+
+	@Override
+	public List<CategoryReponse> getActiveCategory() {
+
+		List<Category> categories = categoryRepo.findByIsActiveTrueAndIsDeletedFalse();
+		List<CategoryReponse> categoryList = categories.stream().map(cat -> mapper.map(cat, CategoryReponse.class))
+				.toList();
+		return categoryList;
+	}
+
+	@Override
+	public CategoryDto getCategoryById(Integer id) {
+
+		Optional<Category> findByCatgeory = categoryRepo.findByIdAndIsDeletedFalse(id);
+
+		if (findByCatgeory.isPresent()) {
+			Category category = findByCatgeory.get();
+			return mapper.map(category, CategoryDto.class);
+		}
+		return null;
+	}
+
+	@Override
+	public Boolean deleteCategory(Integer id) {
+		Optional<Category> findByCatgeory = categoryRepo.findById(id);
+
+		if (findByCatgeory.isPresent()) {
+			Category category = findByCatgeory.get();
+			category.setIsDeleted(true);
+			categoryRepo.save(category);
+			return true;
+		}
+		return false;
+	}
+
+}
